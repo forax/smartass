631e502cf02077b586da5271a0477edb08b36ec5
forax <forax@xxx.xxx>
2014-04-21 21:07:14 +0200

    remove unused import

--- src/com/github/forax/smartass/rt/Klass.java
+++ src/com/github/forax/smartass/rt/Klass.java
@@ -5,7 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 public final class Klass {
   private final String name;
